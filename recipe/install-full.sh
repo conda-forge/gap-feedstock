48761fc4a69e2bc68fe3d5731f6872076546ec17
Isuru Fernando <isuruf@gmail.com>
2019-06-16 13:58:40 -0500

    Only 3 packages

---
+++ recipe/install-full.sh
@@ -0,0 +1,5 @@
+INSTALL_DIR="$PREFIX/gap"
+
+rm -rf "$INSTALL_DIR/pkg"
+
+mv pkg $INSTALL_DIR/pkg
---
+++ recipe/install-minimal.sh
@@ -0,0 +1,25 @@
+INSTALL_DIR="$PREFIX/gap"
+
+mkdir -p "$INSTALL_DIR/pkg"
+
+cd pkg
+
+for GAP_PKG_NAME in smallgrp transgrp primgrp gapdoc;
+do
+    PKG_DIR=`find . -maxdepth 1 -iname "$GAP_PKG_NAME-*" -type d`
+
+    if [[ -z "$PKG_DIR" ]]; then
+        PKG_DIR=`find . -maxdepth 1 -iname "$GAP_PKG_NAME" -type d`
+    fi
+
+    if [[ -z "$PKG_DIR" ]]; then
+        PKG_DIR=`find . -maxdepth 1 -iname "$GAP_PKG_NAME[0-9]*" -type d`
+    fi
+
+    if [[ -z "$PKG_DIR" ]]; then
+        PKG_DIR=`find . -maxdepth 1 -iname "$GAP_PKG_NAME*" -type d`
+    fi
+
+    mv $PKG_DIR $INSTALL_DIR/pkg/$PKG_DIR
+    ln -s "$INSTALL_DIR/pkg/$PKG_DIR" "$PKG_DIR"
+done
