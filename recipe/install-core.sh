f6cd9dfd0ad6e583fa3c52901cd765558a08a998
Isuru Fernando <isuruf@gmail.com>
2019-06-17 02:34:17 -0500

    gap-core to match debian

--- recipe/install-core.sh
+++ recipe/install-core.sh
@@ -13,3 +13,24 @@ GAP_SRC_PATH=`ls -d "$INSTALL_DIR"/bin/*/src`
 rm "$GAP_SRC_PATH"
 ln -s "$INSTALL_DIR"/src/ "$GAP_SRC_PATH"
 
+
+mkdir -p "$INSTALL_DIR/pkg"
+cd pkg
+for GAP_PKG_NAME in smallgrp transgrp primgrp gapdoc;
+do
+    PKG_DIR=`find . -maxdepth 1 -iname "$GAP_PKG_NAME-*" -type d`
+
+    if [[ -z "$PKG_DIR" ]]; then
+        PKG_DIR=`find . -maxdepth 1 -iname "$GAP_PKG_NAME" -type d`
+    fi
+
+    if [[ -z "$PKG_DIR" ]]; then
+        PKG_DIR=`find . -maxdepth 1 -iname "$GAP_PKG_NAME[0-9]*" -type d`
+    fi
+
+    if [[ -z "$PKG_DIR" ]]; then
+        PKG_DIR=`find . -maxdepth 1 -iname "$GAP_PKG_NAME*" -type d`
+    fi
+
+    mv $PKG_DIR $INSTALL_DIR/pkg/$PKG_DIR
+done
