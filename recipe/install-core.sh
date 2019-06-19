5776a6b16296175f138c9a50858faeca547b5933
Isuru Fernando <isuruf@gmail.com>
2019-06-19 09:48:25 +0200

    debug

--- recipe/install-core.sh
+++ recipe/install-core.sh
@@ -38,5 +38,8 @@ cd pkg
 for GAP_PKG_NAME in smallgrp transgrp primgrp gapdoc;
 do
     PKG_DIR=`find . -maxdepth 1 -iname "$GAP_PKG_NAME-*" -type d`
+    echo "GAP_PKG_NAME: $GAP_PKG_NAME"
+    echo "PKG_DIR: $PKG_DIR"
+    ls -al .
     mv $PKG_DIR $INSTALL_DIR/pkg/$PKG_DIR
 done
