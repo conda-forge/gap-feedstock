e91f7dc5c15f83c2b7b6a32c7410cd0f95410462
Isuru Fernando <isuruf@gmail.com>
2019-08-05 18:10:51 -0500

    Patch setup.py

--- recipe/install-jupyter.sh
+++ recipe/install-jupyter.sh
@@ -4,7 +4,11 @@ cp $INSTALL_DIR/pkg/JupyterKernel-*/bin/jupyter-kernel-gap .
 sed -i.bak "s@  GAP=gap@  GAP=$PREFIX/bin/gap@g" jupyter-kernel-gap
 cp jupyter-kernel-gap $PREFIX/bin/jupyter-kernel-gap
 
+pushd $INSTALL_DIR/pkg/JupyterKernel-*/
+sed -i.bak "s/user=user/user=user, prefix=sys.prefix/g" setup.py
+rm setup.py.bak
 python setup.py install
+popd
 
 cp $RECIPE_DIR/scripts/post-link.sh $PREFIX/bin/.gap-jupyter-post-link.sh
 cp $RECIPE_DIR/scripts/pre-unlink.sh $PREFIX/bin/.gap-jupyter-pre-unlink.sh
