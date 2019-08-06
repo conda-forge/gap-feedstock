f5b98c97b7c9d7f4d758526e97c7a527d81a8be4
Isuru Fernando <isuruf@gmail.com>
2019-08-05 20:57:12 -0500

    No need of a new package

--- recipe/scripts/post-link.sh
+++
@@ -1,1 +0,0 @@
-"${PREFIX}/bin/jupyter-nbextension" enable gap-mode/main --sys-prefix >> "${PREFIX}/.messages.txt" 2>&1
--- recipe/scripts/pre-unlink.sh
+++
@@ -1,2 +0,0 @@
-"${PREFIX}/bin/jupyter-nbextension" disable nbgrader --py --sys-prefix >> "${PREFIX}/.messages.txt" 2>&1
-
