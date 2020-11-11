7d014c4b745fa8af4c4ea4716630d4954ec1a6d2
Isuru Fernando <isuruf@gmail.com>
2020-11-11 14:28:16 -0600

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -65,7 +65,7 @@ popd
 DIGRAPHS_PKG_DIR=`find . -maxdepth 1 -iname "digraphs-*" -type d`
 pushd $DIGRAPHS_PKG_DIR
   mv VERSION .VERSION
-  sed -i.bak 's@< VERSION@< .VERSION@g' configure.ac
+  #sed -i.bak 's@< VERSION@< .VERSION@g' configure.ac
 popd
 
 sed -i.bak "s@./build-normaliz.sh@echo@g" ../bin/BuildPackages.sh
