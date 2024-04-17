72b13883f99a0419c2df3ce50ee2c170001b3011
Julian Rüth <julian.rueth@fsfe.org>
2024-04-18 02:31:05 +0300

    GAP binary is created during make install now

--- recipe/build.sh
+++ recipe/build.sh
@@ -53,17 +53,3 @@ for file in log/*; do
   cat $file
 done
 rm -rf log
-
-# https://github.com/gap-system/gap/issues/1567
-export TERM=dumb
-
-if [[ "$target_platform" == *-64 ]]; then
-  for folder in *; do
-    pushd $folder
-    GAP_PKG_NAME=$(echo $folder | cut -d- -f1)
-    echo "GAP_PKG_NAME: $GAP_PKG_NAME"
-    load_output=$(../../bin/gap -q -T <<< "LoadPackage(\"$GAP_PKG_NAME\");")
-    [[ "${load_output}" == "true" || "${load_output:1}" == "true" ]] || echo "Loading fails"
-    popd
-  done
-fi
