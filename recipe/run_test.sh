a2bd7f22fdc0673b2e01c76a6c3343496c09097c
Julian Rüth <julian.rueth@fsfe.org>
2024-04-19 02:24:31 +0300

    Do not stop testing when a package fails to load

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -8,6 +8,8 @@ export TERM=dumb
 
 cd $INSTALL_DIR/pkg
 
+broken_packages=()
+
 if [[ "$target_platform" == *-64 ]]; then
   for folder in *; do
     pushd $folder
@@ -15,10 +17,24 @@ if [[ "$target_platform" == *-64 ]]; then
     echo "*******************************************************"
     echo "* Verifying that GAP package $GAP_PKG_NAME loads."
     echo "*******************************************************"
-    gap --nointeract -b -c 'SetInfoLevel(InfoPackageLoading, 4); if LoadPackage("'$GAP_PKG_NAME'") = fail then GapExitCode(1); fi;'
-    echo "*******************************************************"
-    echo "* OK. GAP package $GAP_PKG_NAME loads."
-    echo "*******************************************************"
+    if ! gap --nointeract -b -c 'SetInfoLevel(InfoPackageLoading, 4); if LoadPackage("'$GAP_PKG_NAME'") = fail then GapExitCode(1); fi;'; then
+      broken_packages+=("$GAP_PKG_NAME")
+      echo "*******************************************************"
+      echo "* Error. GAP package $GAP_PKG_NAME does not load."
+      echo "*******************************************************"
+    else
+      echo "*******************************************************"
+      echo "* OK. GAP package $GAP_PKG_NAME loads."
+      echo "*******************************************************"
+    fi
     popd
   done
+fi
+
+if [ ${#broken_packages[@]} -eq 0 ]; then
+  echo "All GAP packages in this installation seem to be functional."
+else
+  echo "Some GAP packages in this installation are not functional:"
+  printf " - %s\n" "${broken_packages[@]}"
+  exit 1
 fi
