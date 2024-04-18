b86eac0f42b9d551fbe8512c7a5d4b32df4496cc
Julian Rüth <julian.rueth@fsfe.org>
2024-04-18 15:13:41 +0300

    Fail tests if package does not load

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 INSTALL_DIR="$PREFIX/share/gap"
 
@@ -11,9 +12,14 @@ if [[ "$target_platform" == *-64 ]]; then
   for folder in *; do
     pushd $folder
     GAP_PKG_NAME=$(echo $folder | cut -d- -f1)
-    echo "GAP_PKG_NAME: $GAP_PKG_NAME"
+    echo "*******************************************************"
+    echo "* Verifying that GAP package $GAP_PKG_NAME loads."
+    echo "*******************************************************"
     load_output=$(gap -q -T <<< "LoadPackage(\"$GAP_PKG_NAME\");")
-    [[ "${load_output}" == "true" || "${load_output:1}" == "true" ]] || echo "Loading fails"
+    [[ "${load_output}" == "true" || "${load_output:1}" == "true" ]]
+    echo "*******************************************************"
+    echo "* OK. GAP package $GAP_PKG_NAME loads."
+    echo "*******************************************************"
     popd
   done
 fi
